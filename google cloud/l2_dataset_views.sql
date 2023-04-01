eb91ff8c231d0ac4d419f2ed955bfbeea32c0401
Donnell Jackson <55963911+DonnieData@users.noreply.github.com>
2023-04-01 00:44:31 -0500

    Update and rename l2_dataset_views.sql to l2_dataset_tables.sql

--- google cloud/l2_dataset_views.sql
+++
@@ -1,7 +0,0 @@
--- view for service_requests by type
-create view `qwiklabs-gcp-03-78402af0367f.l2_datasets.service_type_dept` as (
-  select department, count(service_request_number) service_count from `qwiklabs-gcp-03-78402af0367f.l1_datasets.311_data`
-  group by department 
-  order by service_count desc
-)
-
