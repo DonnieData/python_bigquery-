14342a5c53eeada7ac92878b69be4da394f4bcb7
Donnell Jackson <55963911+DonnieData@users.noreply.github.com>
2023-04-01 00:27:10 -0500

    Update l2_dataset_views.sql

--- google cloud/l2_dataset_views.sql
+++ google cloud/l2_dataset_views.sql
@@ -1,6 +1,7 @@
--- for metrics 
-select * from `qwiklabs-gcp-03-78402af0367f.l1_datasets.311_data`
-where created_date between '2022-01-01' and '2022-02-23' order by created_date desc;
-
-
+-- view for service_requests by type
+create view `qwiklabs-gcp-03-78402af0367f.l2_datasets.service_type_dept` as (
+  select department, count(service_request_number) service_count from `qwiklabs-gcp-03-78402af0367f.l1_datasets.311_data`
+  group by department 
+  order by service_count desc
+)
 
