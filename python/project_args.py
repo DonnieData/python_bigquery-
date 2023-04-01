dfa7ab1f663854a63996106a1c072e3d9e36f07d
donniedata <donnell.jackson@outlook.com>
2023-03-31 21:41:57 -0500

    creating script to extract and load data into warehouse

---
+++ python/project_args.py
@@ -0,0 +1,5 @@
+args = {
+    "gcp_project_id": "'qwiklabs-gcp-00-9ff4d5371eda",
+    "api_endpoint": "https://www.dallasopendata.com/resource/d7e7-envw.json?$limit=20&$where=created_date between '2022-01-01T00:00:00.000' and '2022-12-31T23:59:59.000'"
+
+}
