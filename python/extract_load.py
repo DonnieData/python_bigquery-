f1cf81f26518ab76ed1ff0a64a1fd2a41a318078
donniedata <donnell.jackson@outlook.com>
2023-03-31 21:47:08 -0500

    updating files

--- python/extract_load.py
+++ python/extract_load.py
@@ -18,10 +18,10 @@ def main(project_id, endpoint):
     data = requests.get(url=endpoint)
     
     l1_df = pd.DataFrame(data.json())
+    print(len(l1_df))
     
     l1_df.to_gbq(f'{project_id}.l1_datasets.311_data', if_exists='replace')
     
-    print(len(l1_df))
     
 if __name__ == "__main__":
     main(project_args.args["gcp_project_id"], project_args.args["api_endpoint"] )
