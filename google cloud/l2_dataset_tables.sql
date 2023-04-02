18a0ccae56d67555e9817ee8cfab803fed124d1e
Donnell Jackson <55963911+DonnieData@users.noreply.github.com>
2023-04-01 19:04:17 -0500

    Update l2_dataset_tables.sql

--- google cloud/l2_dataset_tables.sql
+++ google cloud/l2_dataset_tables.sql
@@ -1,20 +1,6 @@
--- view for service_requests by type
-create table `qwiklabs-gcp-03-78402af0367f.l2_datasets.service_type_dept` as (
-  select department, count(service_request_number) service_count from `qwiklabs-gcp-03-78402af0367f.l1_datasets.311_data`
-  group by department 
-  order by service_count desc
-)
-
--- view for service_requests by status
-create table `qwiklabs-gcp-03-78402af0367f.l2_datasets.service_by_status` as (
-  select status, count(service_request_number) service_count from `qwiklabs-gcp-03-78402af0367f.l1_datasets.311_data`
-  group by status 
-  order by service_count desc
-)
-
-
+--L2
 --no timezone imforation provided so datetime used instead of timestamp to prevent from converting ot UTC
-create table `[project_id].l1_tables.base_table` as (
+create table `[project_id].l2_tables.311_transformed` as (
 select 
 * 
 ,datetime(created_date) created_date_t
@@ -23,7 +9,29 @@ select
 ,ifnull(datetime(closed_date),current_datetime()) closed_date_t
 ,right(address,5) zipcode
 
-from `[project_id].l2_tables.311_data`
+from `[project_id].l1_tables.311_data`
 )
+
+--L3
+-- table for service_requests by department
+create table `[project_id].l3_datasets.service_type_dept` as (
+  select department, count(service_request_number) service_count from `[project_id].l2_tables.311_transformed`
+  group by department 
+  order by service_count desc
+)
+
+-- table for service_requests by status
+create table `[project_id].l3_tables.service_by_status` as (
+  select status, count(service_request_number) service_count from `[project_id].l2_tables.311_transformed`
+  group by status 
+  order by service_count desc
+)
+
+-- service_requests count by zipcode 
+select zipcode, count(service_request_number)
+from `donni12.l1_data.base_table`
+group by zipcode 
+
+
 
 
