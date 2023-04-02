f92f06e0abde2e08f61d6633325cc2fb450ad164
Donnell Jackson <55963911+DonnieData@users.noreply.github.com>
2023-04-02 13:20:02 -0500

    Update l2_dataset_tables.sql

--- google cloud/l2_dataset_tables.sql
+++ google cloud/l2_dataset_tables.sql
@@ -8,7 +8,17 @@ select
 ,datetime(update_date) update_date_t
 ,ifnull(datetime(closed_date),current_datetime()) closed_date_t
 ,right(address,5) zipcode
-
+,case 
+when status = 'Closed' then 'Closed'
+when status = 'Closed (Transferred)' then 'Closed'
+when status = 'Closed (Duplicate)' then 'Closed'
+when status = 'In Progress' then 'Open'
+when status = 'New' then 'Open'
+when status = 'In Progress (Duplicate)' then 'Open'
+when status = 'New (Duplicate)' then 'Open'
+when status = 'On Hold' then 'Open'
+when status = 'On Hold (Duplicate)' then 'Open'
+end status_second
 from `[project_id].l1_tables.311_data`
 )
 
@@ -22,8 +32,8 @@ create table `[project_id].l3_datasets.service_request_by_dept` as (
 
 -- table for service_requests by status
 create table `[project_id].l3_tables.service_by_status` as (
-  select status, count(service_request_number) service_count from `[project_id].l2_tables.311_transformed`
-  group by status 
+  select status_second, status, count(service_request_number) service_count from `[project_id].l2_tables.311_transformed`
+  group by status_second, status 
   order by service_count desc
 )
 
